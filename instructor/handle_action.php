636da5db2a2fde52d96949921a6d0e7000d2e7c3
kaleb-kebede <developer.kaleb@gmail.com>
2025-04-24 10:07:38 +0300

    added a case for insrtuctor's action to handle manage questions functionality

--- instructor/handle_action.php
+++ instructor/handle_action.php
@@ -98,8 +98,11 @@ if (isset($_GET['action'])) {
                 case 'instructor_edit_exam':
                     include "edit_exam.php";
                     break;
-                    case 'instructor_edit_exam_submit':
+                case 'instructor_edit_exam_submit':
                     include "process_edit_exam.php";
+                    break;
+                case 'instructor_manage_questions':
+                    include "manage_questions.php";
                     break;
                 case 'instructor_exam_report':
                     
