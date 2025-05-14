14b210c8ee1050d4758cc3b650d575a783ad484c
codermiki <mikiastadessemiki@gmail.com>
2025-05-14 13:17:36 +0300

    database export

--- includes/db/online_exam_db_old2.sql
+++ includes/db/online_exam_db_old2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2025 at 12:15 PM
+-- Generation Time: May 14, 2025 at 01:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,13 +35,6 @@ CREATE TABLE `assigned_instructors` (
   `status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `assigned_instructors`
---
-
-INSERT INTO `assigned_instructors` (`id`, `instructor_id`, `course_id`, `assigned_on`, `status`) VALUES
-(14, 'INST1004', 'CS101', '2025-05-14 08:50:33', 'Active');
-
 -- --------------------------------------------------------
 
 --
@@ -55,15 +48,6 @@ CREATE TABLE `assigned_students` (
   `assigned_on` datetime DEFAULT current_timestamp(),
   `status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `assigned_students`
---
-
-INSERT INTO `assigned_students` (`id`, `student_id`, `course_id`, `assigned_on`, `status`) VALUES
-(17, 'WCU1501120', 'CS101', '2025-05-14 08:40:00', 'Active'),
-(18, 'WCU1501121', 'CS101', '2025-05-14 10:21:59', 'Active'),
-(19, 'WCU1501122', 'CS101', '2025-05-14 10:21:59', 'Active');
 
 -- --------------------------------------------------------
 
@@ -83,9 +67,7 @@ CREATE TABLE `courses` (
 
 INSERT INTO `courses` (`course_id`, `course_name`, `created_at`) VALUES
 ('CS101', 'Introduction to Programming', '2025-05-13 10:18:05'),
-('CS102', 'Data Structures', '2025-05-14 08:42:02'),
-('CS103', 'Database Systems', '2025-05-13 10:19:44'),
-('CS105', 'Networking', '2025-05-14 08:42:02');
+('CS103', 'Database Systems', '2025-05-13 10:19:44');
 
 -- --------------------------------------------------------
 
@@ -98,19 +80,11 @@ CREATE TABLE `exams` (
   `course_id` varchar(50) NOT NULL,
   `instructor_id` varchar(50) NOT NULL,
   `exam_title` varchar(100) NOT NULL,
-  `exam_description` text DEFAULT NULL,
   `duration_minutes` int(11) NOT NULL,
   `total_marks` int(11) NOT NULL,
   `status` enum('Active','Inactive') DEFAULT 'Inactive',
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `exams`
---
-
-INSERT INTO `exams` (`exam_id`, `course_id`, `instructor_id`, `exam_title`, `exam_description`, `duration_minutes`, `total_marks`, `status`, `created_at`) VALUES
-(8, 'CS101', 'INST1004', 'C++ Midterm Exam', 'mid examination for software engineering department', 10, 10, 'Active', '2025-05-14 09:52:56');
 
 -- --------------------------------------------------------
 
@@ -124,13 +98,6 @@ CREATE TABLE `exam_schedules` (
   `scheduled_date` datetime NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `exam_schedules`
---
-
-INSERT INTO `exam_schedules` (`schedule_id`, `exam_id`, `scheduled_date`, `created_at`) VALUES
-(11, 8, '2025-05-14 10:25:00', '2025-05-14 09:57:01');
 
 -- --------------------------------------------------------
 
@@ -146,13 +113,6 @@ CREATE TABLE `feedbacks` (
   `rate` int(11) DEFAULT NULL CHECK (`rate` between 1 and 5),
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `feedbacks`
---
-
-INSERT INTO `feedbacks` (`id`, `student_id`, `exam_id`, `feedback_text`, `rate`, `created_at`) VALUES
-(8, 'WCU1501120', 8, 'i`m truble to work on this exam', 1, '2025-05-14 10:20:47');
 
 -- --------------------------------------------------------
 
@@ -170,15 +130,6 @@ CREATE TABLE `questions` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `questions`
---
-
-INSERT INTO `questions` (`question_id`, `exam_id`, `question_text`, `question_type`, `correct_answer`, `marks`, `created_at`) VALUES
-(13, 8, 'c++ is compiled programing languege', 'true_false', 'true', 2, '2025-05-14 09:52:56'),
-(14, 8, 'what is the built in function used to display', 'multiple_choice', 'option_2', 3, '2025-05-14 10:17:19'),
-(15, 8, '________ is c++ key word used to out from the swich', 'fill_blank', 'break', 5, '2025-05-14 10:17:19');
-
 -- --------------------------------------------------------
 
 --
@@ -190,16 +141,6 @@ CREATE TABLE `question_options` (
   `question_id` int(11) NOT NULL,
   `option_text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `question_options`
---
-
-INSERT INTO `question_options` (`option_id`, `question_id`, `option_text`) VALUES
-(17, 14, 'endl'),
-(18, 14, 'cout'),
-(19, 14, 'cat'),
-(20, 14, 'log');
 
 -- --------------------------------------------------------
 
@@ -217,16 +158,6 @@ CREATE TABLE `student_answers` (
   `answered_on` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `student_answers`
---
-
-INSERT INTO `student_answers` (`id`, `student_id`, `exam_id`, `question_id`, `answer_text`, `is_correct`, `answered_on`) VALUES
-(31, 'WCU1501120', 8, 13, 'True', 0, '2025-05-14 10:02:50'),
-(32, 'WCU1501121', 8, 14, 'cout', 0, '2025-05-14 10:25:12'),
-(33, 'WCU1501121', 8, 15, 'break', 1, '2025-05-14 10:25:27'),
-(34, 'WCU1501121', 8, 13, 'True', 0, '2025-05-14 10:25:36');
-
 -- --------------------------------------------------------
 
 --
@@ -241,14 +172,6 @@ CREATE TABLE `student_exam_status` (
   `score` float DEFAULT NULL,
   `taken_on` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `student_exam_status`
---
-
-INSERT INTO `student_exam_status` (`id`, `student_id`, `exam_id`, `has_taken`, `score`, `taken_on`) VALUES
-(10, 'WCU1501120', 8, 1, 0, '2025-05-14 10:02:50'),
-(11, 'WCU1501121', 8, 1, 5, '2025-05-14 10:25:36');
 
 -- --------------------------------------------------------
 
@@ -266,19 +189,6 @@ CREATE TABLE `users` (
   `status` enum('Active','Inactive') DEFAULT 'Active',
   `must_reset_password` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `role`, `created_at`, `status`, `must_reset_password`) VALUES
-('1', 'System Admin', 'admin@example.com', '$2y$10$MJYyJT.V4S267hNTahN5s.4ixtXwqlBGa.sE7Zb1zsan0BPxkj/66', 'Admin', '2025-05-14 08:36:21', 'Active', 0),
-('2', 'Jane Instructor', 'instructor@example.com', '$2y$10$PRdZBDVh2S6et1riN2.lSOJKg76WzpDSvPAiPWWldTzsy5IyzEfya', 'Instructor', '2025-05-14 08:36:21', 'Active', 1),
-('3', 'John Student', 'student@example.com', '$2y$10$PRdZBDVh2S6et1riN2.lSOJKg76WzpDSvPAiPWWldTzsy5IyzEfya', 'Student', '2025-05-14 08:36:21', 'Active', 1),
-('INST1004', 'Abel Tadesse', 'abel@university.edu', '$2y$10$eM0QsDYFT3C9mwwVdsVwHu0SZ1zGwY/XVT28bRDTXD2nHIymD31J.', 'Instructor', '2025-05-14 08:50:33', 'Active', 0),
-('WCU1501120', 'Eva White', 'eva.white@example.com', '$2y$10$Vehkx/VXIXwgPu6t0dl/r.g/3keIB0F5cLg0m6Ikv/FtxodgPwI2m', 'Student', '2025-05-14 08:40:00', 'Active', 0),
-('WCU1501121', 'Eva Black', 'eva.black@example.com', '$2y$10$3riQvxb7KEUen4CGTbi.BOPqX4m4EzRKfmqI7bUclG6ZMnWaEZWm.', 'Student', '2025-05-14 10:21:59', 'Active', 0),
-('WCU1501122', 'Eva Black', 'eva.red@example.com', '$2y$10$mps9IQiWtKS5.GrQccEy5OtWSErTgJSfd1CnxGg9OO5wrBnKqE53C', 'Student', '2025-05-14 10:21:59', 'Active', 1);
 
 --
 -- Indexes for dumped tables
@@ -376,55 +286,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assigned_instructors`
 --
 ALTER TABLE `assigned_instructors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `assigned_students`
 --
 ALTER TABLE `assigned_students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `exam_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `exam_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `exam_schedules`
 --
 ALTER TABLE `exam_schedules`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `question_options`
 --
 ALTER TABLE `question_options`
-  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `option_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `student_answers`
 --
 ALTER TABLE `student_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `student_exam_status`
 --
 ALTER TABLE `student_exam_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
